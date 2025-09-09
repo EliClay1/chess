ce22bc5be8ca7d21a2d25c5d7250bd7bc9cc1934
Eli Clayson <eliclay1.byu.edu>
2025-09-09 09:19:53 -0600

    Added PieceLogicHelper.isWithinBoardBounds helper function. Planned out next steps.

--- shared/src/main/java/chess/PieceLogicHelper.java
+++ shared/src/main/java/chess/PieceLogicHelper.java
@@ -9,15 +9,20 @@ public class PieceLogicHelper {
         if (typeOfPiece == ChessPiece.PieceType.KING) {
             /* Can move any direction by one space.
             * This means that there are 8 possible directions that it can move in one turn.
-            * Take the current position, add 1 to the y-axis (if board is within that range), and mark that position.
             *
-            * */
-
-            System.out.println(currentPosition);
-
-            /* 9/9/2025 - Changed to ArrayList because listOfMoves.add was complaining, and when running the code, I ran into
-            some immutable errors, not exactly sure what is going on here.
+            * 9/9/2025 - Changed to ArrayList because listOfMoves.add was complaining, and when running the code, I ran into
+            * some immutable errors, not exactly sure what is going on here.
              */
+
+            /*
+            * TODO - Check to ensure that the next possible move is within the bounds of the board. Could build
+            *  a helper function as this will be something that needs to be checked with every piece type.
+            *
+            * TODO - Check to see if there is already a piece in a possible position. Save that position to a separate
+            *  list, if the user chooses to move there, piece-taking functionality will go into play. There will be
+            *  an additional helper function to cover all of that.
+            */
+
             List<ChessMove> listOfMoves = new java.util.ArrayList<>(List.of());
 
             for (int x = currentPosition.getRow() - 1; x <= currentPosition.getRow() + 1; x++) {
@@ -56,5 +61,12 @@ public class PieceLogicHelper {
 
 
         throw new RuntimeException("Non-designed / Created function");
+    }
+
+    public boolean isWithinBoardBounds(ChessBoard board, ChessPosition positionToCheck) {
+        // changed to -1 to account for the fact that the board goes 1 - 8, while the array goes 0 - 7
+        int boardDimensions = board.gameBoard.length - 1;
+        return positionToCheck.getRow() <= boardDimensions && positionToCheck.getColumn() <= boardDimensions
+                && positionToCheck.getRow() > 0 && positionToCheck.getColumn() > 0;
     }
 }
