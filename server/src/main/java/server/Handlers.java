4e741293c8b05196ab5bb39af7bf44803e84350d
Eli Clayson <eliclay1.byu.edu>
2025-10-11 11:20:56 -0600

    base input validation implemented

--- server/src/main/java/server/Handlers.java
+++ server/src/main/java/server/Handlers.java
@@ -1,6 +1,8 @@
 package server;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
 import io.javalin.http.Context;
 
 import java.util.Map;
@@ -11,13 +13,40 @@ public class Handlers {
     void register(Context ctx) {
         var serializer = new Gson();
         String requestJson = ctx.body();
-
         var request = serializer.fromJson(requestJson, Map.class);
+        // check for inputValidation
+        areInputsValid(request);
 
         // call to the service and register
 
         // this is targeted for specifically register, but this needs to be generalized for any kind of input. This is the HANDLER.
         var response = Map.of("username", request.get("username"), "authToken", UUID.randomUUID().toString());
         ctx.result(serializer.toJson(response));
+    }
+
+    public static <K, V> void areInputsValid(Map<K, V> inputMap) {
+        // currently this only checks if the username is "username", etc.
+        for (var key : inputMap.keySet()) {
+            var value = inputMap.get(key);
+            if (value == null) {
+                System.out.println("ERROR: YOU ARE MISSING SOME KIND OF VALUE!");
+            } else if (value instanceof String && ((String) value).isEmpty()) {
+                System.out.println("ERROR: YOU ARE MISSING SOME KIND OF VALUE!");
+            }
+            // TODO: Check for sanitization, preventing SQL injection. Not required, but would be cool to add...
+        }
+    }
+
+    // code copied from spec
+    void serializeGame() {
+        var serializer = new Gson();
+
+        var game = new ChessGame();
+
+// serialize to JSON
+        var json = serializer.toJson(game);
+
+// deserialize back to ChessGame
+        game = serializer.fromJson(json, ChessGame.class);
     }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,27 +1,28 @@
 package server;
 
-import com.google.gson.Gson;
 import io.javalin.*;
-import io.javalin.http.Context;
-
-import java.util.Map;
-import java.util.UUID;
 
 public class Server {
 
     private final Javalin javalinServer;
-    private Handlers handlers;
 
 
     public Server() {
-        handlers = new Handlers();
+        Handlers handlers = new Handlers();
         javalinServer = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
 
         javalinServer.delete("db", ctx -> ctx.result("{}"));
         // ctx -> register(ctx) ===== this::register
-        javalinServer.post("user", ctx -> handlers.register(ctx));
+        // TODO: REGISTER
+        javalinServer.post("user", handlers::register);
+
+        // TODO: LOGIN
+//        javalinServer.post("user", handlers::register);
+
+        // TODO:
+//        javalinServer.post("user", handlers::register);
     }
 
     public int run(int desiredPort) {
