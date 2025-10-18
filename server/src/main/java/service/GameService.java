aa2ca0b4deecf2b9871f0da53c705a43219a42fe
Eli Clayson <eliclay1.byu.edu>
2025-10-18 11:12:15 -0600

    create game implemented, tests wip

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -13,17 +13,13 @@ public class GameService {
         this.dataAccess = dataAccess;
     }
 
-    public GameData createGame(String gameName, String authToken) throws Exception {
+    public void createGame(String gameName, String authToken) throws Exception {
         AuthData userByAuth = dataAccess.getAuth(authToken);
         // no auth token
         if (userByAuth == null) {
             throw new InvalidException();
         }
-        String username = userByAuth.username();
         GameData newGame = new GameData(dataAccess.createID(), null, null, gameName, new ChessGame());
         dataAccess.createGame(newGame);
-
-        return null;
     }
-
 }
---
+++ server/src/test/java/service/GameServiceTest.java
@@ -0,0 +1,17 @@
+package service;
+
+import model.AuthData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceTest {
+
+    @Test
+    void createGame() {
+        String gameName = "game1";
+        AuthData auth = new AuthData("bob", "1234567890");
+
+    }
+
+}
