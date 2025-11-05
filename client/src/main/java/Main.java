11affff5f8a3693de2e4c000eebb83c3be4ad1a4
Eli Clayson <eliclay1.byu.edu>
2025-11-05 16:49:08 -0700

    First CLI experiment!

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,17 @@
 import chess.*;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        boolean isActive = true;
+        while (isActive) {
+            System.out.printf("Type your numbers%n>>> ");
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+//            var numbers = line.split(" ");
+        }
     }
 }
